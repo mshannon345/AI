7c0af89335ea6e4cd4aaa3e61df5ef68d200adfb
Shannon <milesshannon151@gmail.com>
2024-03-14 13:17:10 -0400

    Added for loop called count - printing numbers 5-20- fixed

--- Count1-10.py
+++ Count1-10.py
@@ -4,5 +4,5 @@
 #Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
 
 # Loop through numbers 1 to 10 and print each number
-for num in range(1, 11):
+for num in range(5,21):
     print(num)
